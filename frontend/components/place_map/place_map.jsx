4334ec0bd1970127768236e32101a9ff62daa6f5
RyanMora <ryan.aimco@gmail.com>
2017-05-25 17:12:26 -0700

    starting placemap over to see if i can get map to show up

--- frontend/components/place_map/place_map.jsx
+++ frontend/components/place_map/place_map.jsx
@@ -2,76 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { withRouter } from 'react-router-dom';
 
-import MarkerManager from '../../util/marker_manager'
-
-const getCoordsObj = latLng => ({
-  lat: latLng.lat(),
-  lng: latLng.lng()
-});
-
-const mapOptions = {
-  center: {
-    lat: 37.773972,
-    lng: -122.431297
-  }, //sf coords
-  zoom: 13
-};
-
 class PlaceMap extends React.Component {
-  componentDidMount() {
-    const map = this.refs.map;
-    this.map = new google.maps.Map(map, mapOptions);
-    this.MarkerManager = new MarkerManager(this.map, this.handleMarkerClick.bind(this));
-    if(this.props.singlePlace){
-      this.props.fetchPlace(this.props.placeId);
-    }else{
-      this.registerListeners();
-      this.MarkerManager.updateMarkers(this.props.places);
-    }
-  }
-
-  componentDidUpdate(){
-    if(this.props.singlePlace){
-      const targetPlaceKey = Object.keys(this.props.places)[0];
-      const targetPlace = this.props.places[targetPlaceKey];
-      this.MarkerManager.updateMarkers([targetPlace]);
-    }else{
-      this.MarkerManager.updateMarkers(this.props.places);
-    }
-  }
-
-  registerListeners(){
-    google.maps.event.addListener(this.map, 'idle', () => {
-      const {north, south, east, west} = this.map.getBounds().toJSON();
-      const bounds = {
-        northEast: {lat: north, lng: east},
-        southWest: {lat: south, lng: west}
-      };
-      this.props.updateFilter('bounds', bounds);
-    });
-    google.maps.event.addListener(this.map, 'click', (event) => {
-      const coords = getCoordsObj(event.latLng);
-      this.handleClick(coords);
-    })
-  }
-
-  handleMarkerClick(place) {
-    this.props.history.push(`places/${place.id}`);
-  }
-
-  handleClick(coords){
-    this.props.history.push({
-      pathname: 'places/new',
-      search: `lat=${coords.lat}&lng=${coords.lng}`
-    });
+  componentDidMount(){
+    const mapOptions = {
+      center: {lat: 37.7758, lng: -122.435 },
+      zoom: 13
+    };
+    const map = this.map;
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
   }
 
   render(){
-    return(
-      <div>
-        Map
-      </div>
-    );
+    return (
+      <div ref={ map=> this.mapNode = map}>OH HIIIIII</div>
+    )
   }
 }
 
