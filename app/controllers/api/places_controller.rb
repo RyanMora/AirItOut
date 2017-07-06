9ab3d678852a8afb42a20a390dd7eced89409488
RyanMora <ryan.aimco@gmail.com>
2017-07-06 09:52:13 -0700

    updated places controller

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,32 +1,23 @@
 class Api::PlacesController < ApplicationController
+
   def index
-    places = Place.all
-    @places = places.includes(:reviews, :favorite_users)
+    @places = Place.all
+    if params[:query]
+      @places = Place.search(params[:query], @places)
+    elsif params[:price] && params[:price].any?{|k, v| v == "true"}
+      prices = []
+      params[:price].each do |k, v|
+        prices.push(k.to_i) if v == "true"
+      end
+
+      @places = Place.where('price IN (?)', prices)
+    end
     render :index
   end
 
   def show
     @place = Place.find(params[:id])
-  end
-
-  def create
-    @place = Place.create!(place_params)
     render :show
   end
 
-  private
-
-  def place_params
-    params.require(:place).permit(
-      :lat,
-      :lng,
-      :description,
-      :type_of_business,
-      :picture_url
-    )
-  end
-  #
-  # def bounds
-  #   params[:bounds]
-  # end
 end
